940ed483a63b32f37836d1d9c2056de4f0873415
Wojtek M <ojciecneo@gmail.com>
2023-10-12 18:52:55 +0200

    more px

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className=" w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 "
+      className=" pt-[50px] w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 "
     >
       <form
         method="POST"

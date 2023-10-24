046b9198ae0b5969501b03fb45d589319709b59c
Wojtek M <ojciecneo@gmail.com>
2023-10-24 07:22:08 +0200

    Cv added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
+import cv from "../assets/CV.pdf";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -128,7 +129,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href={cv}
             >
               CV <BsFillPersonLinesFill size={30} />
             </a>

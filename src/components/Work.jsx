ed4ab154e2a72b1db1cb5a284d76cfc7c6fd2c3b
Wojtek M <ojciecneo@gmail.com>
2023-10-25 11:28:04 +0200

    images added + links connected

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,7 @@
-// import ReactProject from "../assets/project/ReactProject.jpeg";
-import JsProject from "../assets/project/JsProject1.jpg";
+import projectTailwind from "../assets/project/ProjektTailwind01.jpg";
+import projectCss from "../assets/project/ProjektCss01.jpg";
+import projectReact from "../assets/project/Projekt1React.jpg";
+import projectJS from "../assets/project/Projekt1JS.jpg";
 
 const Work = () => {
   return (
@@ -22,19 +24,27 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
           <div
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-            style={{ backgroundImage: `url(${JsProject})` }}
+            style={{ backgroundImage: `url(${projectTailwind})` }}
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React Js App
+                Tailwind Projekt
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://projektbank.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/sn0rii/testProject"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     {/* Code */}
                     Kod
@@ -45,19 +55,27 @@ const Work = () => {
           </div>
           <div
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-            style={{ backgroundImage: `url(${JsProject})` }}
+            style={{ backgroundImage: `url(${projectCss})` }}
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React Js App
+                CSS Projekt
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://projekt-2-future-collars.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/sn0rii/Projekt-2"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     {/* Code */}
                     Kod
@@ -68,19 +86,27 @@ const Work = () => {
           </div>
           <div
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-            style={{ backgroundImage: `url(${JsProject})` }}
+            style={{ backgroundImage: `url(${projectReact})` }}
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React Js App
+                React Projekt
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://react-projekt-2-future.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/sn0rii/react-projekt-2"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     {/* Code */}
                     Kod
@@ -91,19 +117,27 @@ const Work = () => {
           </div>
           <div
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-            style={{ backgroundImage: `url(${JsProject})` }}
+            style={{ backgroundImage: `url(${projectJS})` }}
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React Js App
+                Javascript Projekt
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://1-projekt-js.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/sn0rii/1-PROJEKT-JS"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     {/* Code */}
                     Kod
